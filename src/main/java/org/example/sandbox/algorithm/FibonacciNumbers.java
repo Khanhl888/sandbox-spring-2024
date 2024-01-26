4e23d7624208a36f93b47538276909f578fb6869
Khanhl888 <kha2130096@maricopa.edu>
2024-01-25 18:45:01 -0700

    adding fib

--- src/main/java/org/example/sandbox/algorithm/FibonacciNumbers.java
+++ src/main/java/org/example/sandbox/algorithm/FibonacciNumbers.java
@@ -1,9 +1,18 @@
 package org.example.sandbox.algorithm;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class FibonacciNumbers {
+    private Map<Integer, Long> dictionary;
+
+    public FibonacciNumbers() {
+        this.dictionary = new HashMap<>();
+    }
+
     public static void main(String[] args) {
         FibonacciNumbers fn = new FibonacciNumbers();
-        for(int x=0; x<=15; x++){
+        for(int x=0; x<=55; x++){
             System.out.println("fib("+ x + ") =" + fn.fib(x));
         }
     }
@@ -14,7 +23,21 @@ public class FibonacciNumbers {
              if(n==1){
                  return 1;
             }
-            return fib(n-1)+fib(n-2);
+            return memo(n-1)+fib(n-2);
 
+    }
+
+
+    private long memo(int m){
+
+        Long value = dictionary.get(m);
+
+        if(value!=null){
+            return value;
+        }
+
+        value = fib(m);
+        dictionary.put(Integer.valueOf(m), value);
+        return value;
     }
 }
